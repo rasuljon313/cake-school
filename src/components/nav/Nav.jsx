bfd68acd30bcd5504dc460d52cb632385c63468a
rasuljon313 <anvarovrasuljon313@gmail.com>
2024-11-30 19:11:20 +0500

    add

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,29 @@
+import logo from "../../assets/image/Mask group.svg"
+const Nav = () => {
+  return (
+    <>
+    <header>
+        <div className="nav">
+            <div className="container">
+                <div className="nav_box">
+                    <div className="nav_logo">
+                        <img src={logo} alt="" />
+                    <p>Онлайн школа для кондитеров</p> 
+                    </div>
+                    <ul className="nav_list">
+                        <li className="nav_item">Акции</li>
+                        <li className="nav_item">Отзывы</li>
+                        <li className="nav_item">Техподдержка</li>
+                    </ul>
+                    <button className="nav_btn">
+                    Личный кабинет
+                    </button>
+                </div>
+            </div>
+        </div>
+    </header>
+    </>
+  )
+}
+
+export default Nav
