4ff1c9a8b7e6eea094a7c3ce9a7ecfc9f82da2fa
rasuljon313 <anvarovrasuljon313@gmail.com>
2024-11-30 22:47:31 +0500

    add

--- src/router/App.jsx
+++ src/router/App.jsx
@@ -1,13 +1,13 @@
+import About from "../components/about/About"
 import Header from "../components/header/Header"
 import Nav from "../components/nav/Nav"
 
 const App = () => {
   return (
     <>
-    <div className="s">
       <Nav/>
      <Header/>
-    </div>
+     <About/>
     </>
   )
 }
