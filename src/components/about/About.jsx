7a855c48e1cf85b8f87fb99fbf3a7b6fde434345
rasuljon313 <anvarovrasuljon313@gmail.com>
2024-11-30 22:45:53 +0500

    add

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,33 @@
+import svg1 from "../../assets/image/Mask group.svg"
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <main>
+        <div className="abotu">
+          <div className="container">
+            <div className="about_box">
+              <div className="about_topLayout">
+                <h2 className="about_topLayout_title">Мои уроки</h2>
+                <p className="about_topLayout_txt">для вас, если вы:</p>
+                <ul className="about_list">
+                  <li className="about_item">
+                    <span>
+                      <img src={svg1} alt="" />
+                      <p>Любитель</p>
+                      </span>
+                  </li>
+                  <li className="about_item"></li>
+                  <li className="about_item"></li>
+                  <li className="about_item"></li>
+                </ul>
+              </div>
+              <div className="about_bottomLayout"></div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
 
-export default About
+export default About;
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,4 +1,4 @@
-import logo from "../../assets/image/Mask group.svg"
+import logo from "../../assets/image/1.svg"
 const Nav = () => {
   return (
     <>
