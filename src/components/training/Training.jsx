ca6d756703c5714d91c27eb41d591dc676648f16
rasuljon313 <anvarovrasuljon313@gmail.com>
2024-12-01 17:28:39 +0500

    add

--- src/components/training/Training.jsx
+++ src/components/training/Training.jsx
@@ -4,14 +4,45 @@ import cake2 from "../../assets/image/cake2.svg"
 import table2 from "../../assets/image/table2.png"
 import cake3 from "../../assets/image/cake3.svg"
 import table3 from "../../assets/image/table3.png"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import {Navigation} from 'swiper/modules';
 const Training = () => {
+    const students = [
+        {
+            name:"Насиба",
+            years:"25 лет, Студентка ",
+            txt:"Здравствуйте! Спасибо большое за ваши рецепты! Даже новичок по ним сможет приготовить! Самые вкусные торты у вас!"
+
+        },
+        {
+            name:"Насиба",
+            years:"25 лет, Студентка ",
+            txt:"Здравствуйте! Спасибо большое за ваши рецепты! Даже новичок по ним сможет приготовить! Самые вкусные торты у вас!"
+
+        },
+        {
+            name:"Насиба",
+            years:"25 лет, Студентка ",
+            txt:"Здравствуйте! Спасибо большое за ваши рецепты! Даже новичок по ним сможет приготовить! Самые вкусные торты у вас!"
+
+        },
+        {
+            name:"Насиба",
+            years:"25 лет, Студентка ",
+            txt:"Здравствуйте! Спасибо большое за ваши рецепты! Даже новичок по ним сможет приготовить! Самые вкусные торты у вас!"
+
+        }
+    ]
   return (
     <>
       <section>
         <div className="training">
           <div className="container">
             <div className="training_box">
-              <h3 className="training_title">обучение</h3>
+              <h3 className="training_title">ОБУЧЕНИЕ</h3>
               <p className="training_txt">может быть таким</p>
               <ul className="training_list">
                 <li className="training_item">
@@ -43,6 +74,57 @@ const Training = () => {
                 </li>
                
               </ul>
+            </div>
+          </div>
+        </div>
+        <div className="students">
+          <div className="container">
+            <div className="students_box">
+              <h3 className="students_title">Ученики</h3>
+              <p className="students_txt">довольные результатом обучения</p>
+              <Swiper
+            modules={[Navigation]}
+            spaceBetween={30}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+            loop={true}
+            breakpoints={{
+              1200: {
+                slidesPerView: 3, 
+                spaceBetween: 30, 
+              },
+              1050: {
+                slidesPerView: 3, 
+                spaceBetween: 30, 
+              },
+              1000: {
+                slidesPerView: 3, 
+                spaceBetween: 30, 
+              },
+              750: {
+                slidesPerView: 2, 
+              }
+            }}
+            className="swiper"
+          >
+            {students.map((students, index) => (
+              <SwiperSlide key={index}>
+                <div className="studens_card">
+                  <div className="studens_card_image">
+                    <img src={students.image} alt={students.name} />
+                    <div className="studens-card_details">
+                    <h3>{students.name}</h3>
+                    <p className="studens_card_role">{students.years}</p>
+                    </div>
+                  </div>
+                  <div className="studens_card_content">
+                      <p>{students.txt}</p>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>  
             </div>
           </div>
         </div>
