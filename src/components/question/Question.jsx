0ebc5ecc92649f1bd6585f73b191d571e7dbe82b
rasuljon313 <anvarovrasuljon313@gmail.com>
2024-12-02 09:56:08 +0500

    add

--- src/components/question/Question.jsx
+++ src/components/question/Question.jsx
@@ -4,7 +4,13 @@ const Question = () => {
       <div className="question">
         <div className="conteiner">
             <div className="question_box">
-                
+                <h3 className="question_title">Ответы</h3>
+                <p className="question_txt">на часто задааемые вопросы</p>
+                <ul className="question_list">
+                    <li className="question_item">
+                        
+                    </li>
+                </ul>
             </div>
         </div>
       </div>
