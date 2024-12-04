c996852dd8b2640235cefa3bd5705050f2d24178
rasuljon313 <anvarovrasuljon313@gmail.com>
2024-12-04 07:58:40 +0500

    add

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -3,15 +3,16 @@ const Input = () => {
   return (
     <>
       <form>
-        <div className="imput">
+        <div className="input">
           <div className="container">
             <div className="input_box">
-              <img src={formi} alt="formi" />
+              <div className="input_form">
+                <img src={formi} alt="formi" />
               <div className="input_card">
                 <h3 className="input_title">Напишите нам</h3>
                 <p className="input_support">Режим работы тех.поддержки</p>
                 <p className="input_time">Пн-Сб с 9:00 до 19:00</p>
-                <p className="input_txt">Пн-Сб с 9:00 до 19:00</p>
+                <p className="input_txt">Время ответа в течениие <span>24 часов</span> с момента обращения</p>
                 <div className="input_form">
                     <input type="text" placeholder="Введите ваше имя" />
                     <input type="text" placeholder="Введите ваш телефон" />
@@ -20,7 +21,7 @@ const Input = () => {
                         btn
                     </button>
                 </div>
-              </div>
+              </div></div>
             </div>
           </div>
         </div>
