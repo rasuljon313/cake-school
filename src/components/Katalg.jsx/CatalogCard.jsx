a65e762c62a39c70614fabc8927c2737798e35fc
rasuljon313 <anvarovrasuljon313@gmail.com>
2024-12-05 22:43:52 +0500

    add

--- src/components/Katalg.jsx/CatalogCard.jsx
+++ src/components/Katalg.jsx/CatalogCard.jsx
@@ -2,30 +2,59 @@
 const CatalogCard = () => {
     const obj = [
         {
-            img: {}, 
-            name: "Kurs 1",
-            txt: "Bu kurs haqida qisqacha ma'lumot."
-        },
-        {
-            img: {},
-            name: "Kurs 2",
-            txt: "Bu kurs haqida qisqacha ma'lumot."
-        },
-        {
-            img: {},
-            name: "Kurs 3",
-            txt: "Bu kurs haqida qisqacha ma'lumot."
-        },
-        {
-            img: {},
-            name: "Kurs 4",
-            txt: "Bu kurs haqida qisqacha ma'lumot."
-        },
-        {
-            img: {},
-            name: "Kurs 5",
-            txt: "Bu kurs haqida qisqacha ma'lumot."
-        }
+            title: 'УРОК ПО ПРИГОТОВЛЕНИЮ КАПКЕЙКОВ',
+            description: 'Самые вкусные капкейки с подробным процессом приготовления от и до в одном сборнике! Варианты приготовления капкейков на любой вкус и бюджет.',
+            image: 'https://images.unsplash.com/photo-1587668178277-295251f900ce?w=600&h=400&fit=crop',
+            price: '19,990'
+          },
+          {
+            title: 'ШОКОЛАДНЫЙ ТОРТ',
+            description: 'Классический шоколадный торт с нежным кремом и богатым вкусом какао.',
+            image: 'https://images.unsplash.com/photo-1578985545062-69928b1d9587?w=600&h=400&fit=crop',
+            price: '24,990'
+          },
+          {
+            title: 'ВАНИЛЬНЫЕ КЕКСЫ',
+            description: 'Воздушные ванильные кексы с нежной текстурой и ароматным вкусом.',
+            image: 'https://images.unsplash.com/photo-1557925923-cd4648e211a0?w=600&h=400&fit=crop',
+            price: '15,990'
+          },
+          {
+            title: 'ФРУКТОВЫЙ ТОРТ',
+            description: 'Легкий бисквитный торт со свежими фруктами и йогуртовым кремом.',
+            image: 'https://images.unsplash.com/photo-1464349095431-e9a21285b5f3?w=600&h=400&fit=crop',
+            price: '27,990'
+          },
+          {
+            title: 'МЕДОВЫЙ ТОРТ',
+            description: 'Традиционный медовый торт с нежными коржами и сметанным кремом.',
+            image: 'https://images.unsplash.com/photo-1571115177098-24ec42ed204d?w=600&h=400&fit=crop',
+            price: '23,990'
+          },
+          {
+            title: 'ЛИМОННЫЙ ПИРОГ',
+            description: 'Освежающий лимонный пирог с воздушной меренгой.',
+            image: 'https://images.unsplash.com/photo-1519869325930-281384150729?w=600&h=400&fit=crop',
+            price: '21,990'
+          },
+          {
+            title: 'КЛУБНИЧНЫЕ ТАРТАЛЕТКИ',
+            description: 'Хрустящие тарталетки со свежей клубникой и заварным кремом.',
+            image: 'https://images.unsplash.com/photo-1488477181946-6428a0291777?w=600&h=400&fit=crop',
+            price: '18,990'
+          },
+          {
+            title: 'ТИРАМИСУ',
+            description: 'Классический итальянский десерт с кофейным вкусом и маскарпоне.',
+            image: 'https://images.unsplash.com/photo-1571877227200-a0d98ea607e9?w=600&h=400&fit=crop',
+            price: '26,990'
+          },
+          {
+            title: 'ЭКЛЕРЫ',
+            description: 'Воздушные эклеры с нежным кремом и шоколадной глазурью.',
+            image: 'https://images.unsplash.com/photo-1612203985729-70726954388c?w=600&h=400&fit=crop',
+            price: '20,990'
+          }
     ];
 
     return (
@@ -40,9 +69,12 @@ const CatalogCard = () => {
                                 {obj.map((item, index) => (
                                     <li key={index} className="catalg_item">
                                         <div className="catalg_card">
-                                            <img src={item.img} alt={`Image for ${item.name}`} className="catalg_card_img" />
-                                            <h4 className="catalg_card_title">{item.name}</h4>
-                                            <p className="catalg_card_text">{item.txt}</p>
+                                            <img src={item.image} alt={`Image for ${item.name}`} className="catalg_card_img" />
+                                            <div className="catalg_card_texts">
+                                            <h4 className="catalg_card_title">{item.title}</h4>
+                                            <p className="catalg_card_text">{item.description}</p>
+                                            <p className="catalg_card_price">{item.price}</p>
+                                            </div>
                                         </div>
                                     </li>
                                 ))}
