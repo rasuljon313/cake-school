d9da3dc27242dc23e67b46f73a3e845d8854ac9c
rasuljon313 <anvarovrasuljon313@gmail.com>
2024-12-11 23:26:50 +0500

    Add

--- src/components/Katalg.jsx/CatalgId.jsx
+++ src/components/Katalg.jsx/CatalgId.jsx
@@ -15,9 +15,15 @@ const CatalgId = () => {
         <div className="container">
           <div className="id_box">
             <img src={obg.image} alt={`Image for ${obg.title}`} />
-            <h4>{obg.title}</h4>
+           <div className="id_card">
+           <h4>{obg.title}</h4>
             <p>{obg.description}</p>
             <p>Price: {obg.price}$</p>
+            <div className="id_btns">
+              <button><a href="/form">+</a></button>
+              <button><a href="/catalog">-</a></button>
+            </div>
+           </div>
           </div>
         </div>
       </div>
